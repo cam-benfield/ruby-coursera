9ce3157547d26cda8277d9158012957cc7a83c82
Shelby Benfield <camerynrhosyn@gmail.com>
2019-01-28 18:00:55 -0500

    Lesson 1-2 completed, fails explained

--- module2-lesson1-1.rb
+++ module2-lesson1-1.rb
@@ -1,11 +1,11 @@
 some_var = "false"
 another_var = "nil"
 case
-  when some_var == "pink elephant"
+when some_var == "pink elephant" # fails because the var does not equal "pink elephant"
     puts "Don't think about the pink elephant!"
-  when another_var.nil?
+  when another_var.nil? # fails because the var does not equal nil, to fix, remove quotes
     puts "Question mark in the method name?"
-  when some_var == false
+  when some_var == false # fails for same reasno as above, remove quotes
     puts "Looks like this one should execute"
   else
     puts "I guess nothing matched... But Why?"
