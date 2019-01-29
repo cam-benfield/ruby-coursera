ca19b8706d8187192fb17fe924d9e3c40537263a
Shelby Benfield <camerynrhosyn@gmail.com>
2019-01-29 09:32:02 -0500

    Completed Lesson 1 Assignment, rspec checks out.

--- Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,14 +1,13 @@
-some_var = false
-another_var = nil
-pink_var = "pink elephant"
+some_var = "false"
+another_var = "nil"
 
 case
-when pink_var == "pink elephant" # fails because the var does not equal "pink elephant"
+when some_var == "pink elephant" # fails because the var does not equal "pink elephant"
     puts "Don't think about the pink elephant!"
   when another_var.nil? # fails because the var does not equal nil, to fix, remove quotes
     puts "Question mark in the method name?"
   when some_var == false # fails for same reasno as above, remove quotes
     puts "Looks like this one should execute"
   else
-    puts "I guess nothing matched... But Why?"
+    puts "I guess nothing matched... But why?"
 end
